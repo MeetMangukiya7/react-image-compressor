22314f983eede80246f4981ee9eabbe8776c02d5
Krushang Kasundra <kruzkasu223@gmail.com>
2022-09-29 20:59:18 +0530

    ref: migrated from class component to functional component

--- src/components/ImageCompressor.jsx
+++ src/components/ImageCompressor.jsx
@@ -1,34 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 import imageCompression from "browser-image-compression"
 import Card from "react-bootstrap/Card"
 
-export default class imageCompressor extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      compressedLink: "./placeholder.png",
-      originalImage: "",
-      originalLink: "",
-      clicked: false,
-      uploadImage: false,
-    }
-  }
+export default function ImageCompressor() {
+  const [compressedLink, setCompressedLink] = useState("./placeholder.png")
+  const [originalImage, setOriginalImage] = useState("")
+  const [originalLink, setOriginalLink] = useState("")
+  const [clicked, setClicked] = useState(false)
+  const [uploadImage, setUploadImage] = useState(false)
+  const [outputFileName, setOutputFileName] = useState("")
 
-  handle = (e) => {
+  const handle = (e) => {
     const imageFile = e.target.files[0]
-    this.setState({
-      originalLink: URL.createObjectURL(imageFile),
-      originalImage: imageFile,
-      outputFileName: imageFile.name,
-      uploadImage: true,
-    })
+    setOriginalLink(URL.createObjectURL(imageFile))
+    setOriginalImage(imageFile)
+    setOutputFileName(imageFile.name)
+    setUploadImage(true)
   }
 
-  changeValue = (e) => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
-
-  click = (e) => {
+  const click = (e) => {
     e.preventDefault()
 
     const options = {
@@ -37,92 +27,84 @@ export default class imageCompressor extends React.Component {
       useWebWorker: true,
     }
 
-    if (options.maxSizeMB >= this.state.originalImage.size / 1024) {
+    if (options.maxSizeMB >= originalImage.size / 1024) {
       alert("Image is too small, can't be Compressed!")
       return 0
     }
 
-    let output
-    imageCompression(this.state.originalImage, options).then((x) => {
-      output = x
-
-      const downloadLink = URL.createObjectURL(output)
-      this.setState({
-        compressedLink: downloadLink,
-      })
+    imageCompression(originalImage, options).then((x) => {
+      const downloadLink = URL.createObjectURL(x)
+      setCompressedLink(downloadLink)
     })
 
-    this.setState({ clicked: true })
-    return 1
+    setClicked(true)
   }
 
-  render() {
-    return (
-      <div className="m-5">
-        <div className="text-light text-center">
-          <h1>Three Simple Steps</h1>
-          <h3>1. Upload Image</h3>
-          <h3>2. Click on Compress</h3>
-          <h3>3. Download Compressed Image</h3>
+  return (
+    <div className="m-5">
+      <div className="text-light text-center">
+        <h1>Three Simple Steps</h1>
+        <h3>1. Upload Image</h3>
+        <h3>2. Click on Compress</h3>
+        <h3>3. Download Compressed Image</h3>
+      </div>
+
+      <div className="row mt-5">
+        <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+          {uploadImage ? (
+            <Card.Img
+              className="ht"
+              variant="top"
+              src={originalLink}
+            ></Card.Img>
+          ) : (
+            <Card.Img
+              className="ht"
+              variant="top"
+              src="./placeholder.png"
+            ></Card.Img>
+          )}
+          <div className="d-flex justify-content-center">
+            <input
+              type="file"
+              accept="image/*"
+              className="mt-2 btn btn-dark w-75"
+              onChange={(e) => handle(e)}
+            />
+          </div>
+        </div>
+        <div className="col-xl-4 col-lg-4 col-md-12 mb-5 mt-5 col-sm-12 d-flex justify-content-center align-items-baseline">
+          <br />
+          {outputFileName ? (
+            <button
+              type="button"
+              className=" btn btn-dark"
+              onClick={(e) => click(e)}
+            >
+              Compress
+            </button>
+          ) : (
+            <></>
+          )}
         </div>
 
-        <div className="row mt-5">
-          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
-            {this.state.uploadImage ? (
-              <Card.Img
-                className="ht"
-                variant="top"
-                src={this.state.originalLink}
-              ></Card.Img>
-            ) : (
-              <Card.Img
-                className="ht"
-                variant="top"
-                src="./placeholder.png"
-              ></Card.Img>
-            )}
+        <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 mt-3">
+          <Card.Img variant="top" src={compressedLink}></Card.Img>
+          {clicked ? (
             <div className="d-flex justify-content-center">
-              <input
-                type="file"
-                accept="image/*"
+              <a
+                href={compressedLink}
+                download={outputFileName}
                 className="mt-2 btn btn-dark w-75"
-                onChange={(e) => this.handle(e)}
-              />
-            </div>
-          </div>
-          <div className="col-xl-4 col-lg-4 col-md-12 mb-5 mt-5 col-sm-12 d-flex justify-content-center align-items-baseline">
-            <br />
-            {this.state.outputFileName ? (
-              <button
-                type="button"
-                className=" btn btn-dark"
-                onClick={(e) => this.click(e)}
               >
-                Compress
-              </button>
-            ) : (
-              <></>
-            )}
-          </div>
-
-          <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12 mt-3">
-            <Card.Img variant="top" src={this.state.compressedLink}></Card.Img>
-            {this.state.clicked ? (
-              <div className="d-flex justify-content-center">
-                <a
-                  href={this.state.compressedLink}
-                  download={this.state.outputFileName}
-                  className="mt-2 btn btn-dark w-75"
-                >
-                  Download
-                </a>
-              </div>
-            ) : (
-              <></>
-            )}
-          </div>
+                Download
+              </a>
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
